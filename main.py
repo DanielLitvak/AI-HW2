c1df14e5e5a4a339b06e368c213d8b8408ca4ef7
Daniel Litvak <daniellitvak5@gmail.com>
2024-03-20 19:43:55 +0200

    uncomment time-limit error

--- main.py
+++ main.py
@@ -56,8 +56,8 @@ def run_agents():
                 start = time.time()
                 op = agent.run_step(env, i, args.time_limit)
                 end = time.time()
-                #if end - start > args.time_limit:
-                    #raise RuntimeError(f"Agent used too much time! time used: {end-start}")
+                if end - start > args.time_limit:
+                    raise RuntimeError(f"Agent used too much time! time used: {end-start}")
                 env.apply_operator(i, op)
                 if args.console_print:
                     print('robot ' + str(i) + ' chose ' + op)
